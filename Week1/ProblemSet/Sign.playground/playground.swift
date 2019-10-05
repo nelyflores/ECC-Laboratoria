9a60b5f419610591895d3c2c73b6d6e74d3d01c9
nelyflores <nely.afloresr@gmail.com>
2019-10-05 02:06:39 -0500

    usando switch

---
+++ Week1/ProblemSet/Sign.playground/playground.swift
@@ -0,0 +1,79 @@
+import Foundation
+import GameplayKit
+
+let randomChoice = GKRandomDistribution(lowestValue: 0, highestValue: 2) 
+
+func randomSign() -> Sign {
+  let sign = randomChoice.nextInt()
+	if sign == 0 {
+		return .Piedra
+	} else if sign == 1 {
+		return .Papel
+	} else {
+		return .Tijeras
+  }
+
+
+enum Sign {
+    case Piedra
+    case Papel
+    case Tijeras
+    var emojis: String {
+        switch self {
+        case .Piedra:
+            return "👊🏻"
+        case .Papel:
+            return "🤚"
+        case .Tijeras:
+            return "✌️"
+        }
+    }
+    func giveMeResults(_signComputer: Sign) -> GameState {
+       switch self {
+       case .Piedra:
+        switch signComputer {
+        case .Piedra:
+            return Gamestate.Tie
+        case .Papel
+             return Gamestate.Lose
+        case.Tijeras
+            return Gamestate.Win
+        }
+        
+
+       case .Papel:
+        switch signComputer {
+        case .Piedra:
+            return Gamestate.Win
+        case .Papel
+             return Gamestate.Tie
+        case.Tijeras
+            return Gamestate.Lose
+        }
+
+       case .Tijeras:
+        switch signComputer {
+        case .Piedra:
+            return Gamestate.Lose
+        case .Papel
+             return Gamestate.Win
+        case.Tijeras
+            return Gamestate.Tie
+        }
+     
+       }
+    }
+}
+       
+
+
+
+
+
+
+
+
+
+
+
+}
